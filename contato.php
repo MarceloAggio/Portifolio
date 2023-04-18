ae84d57b85b5a4bec85f8d6d2a070e3563104217
MARCELO DO NASCIMENTO AGGIO <marceloaggio10@gmail.com>
2023-04-18 20:17:02 -0300

    att

--- contato.php
+++ contato.php
@@ -1,46 +1,31 @@
 <?php
-if (isset($_POST['BTEnvia'])) {
- 
- //Variaveis de POST, Alterar somente se necessário 
- //====================================================
- $nome = $_POST['nome'];
- $email = $_POST['email'];
- $telefone = $_POST['telefone']; 
- $mensagem = $_POST['mensagem'];
- //====================================================
- 
- //REMETENTE --> ESTE EMAIL TEM QUE SER VALIDO DO DOMINIO
- //==================================================== 
- $email_remetente = "email@doseudominio"; // deve ser uma conta de email do seu dominio 
- //====================================================
- 
- //Configurações do email, ajustar conforme necessidade
- //==================================================== 
- $email_destinatario = "email@querecebe"; // pode ser qualquer email que receberá as mensagens
- $email_reply = "$email"; 
- $email_assunto = "Contato formmail"; // Este será o assunto da mensagem
- //====================================================
- 
- //Monta o Corpo da Mensagem
- //====================================================
- $email_conteudo = "Nome = $nome \n"; 
- $email_conteudo .= "Email = $email \n";
- $email_conteudo .= "Telefone = $telefone \n"; 
- $email_conteudo .= "Mensagem = $mensagem \n"; 
- //====================================================
- 
- //Seta os Headers (Alterar somente caso necessario) 
- //==================================================== 
- $email_headers = implode ( "\n",array ( "From: $email_remetente", "Reply-To: $email_reply", "Return-Path: $email_remetente","MIME-Version: 1.0","X-Priority: 3","Content-Type: text/html; charset=UTF-8" ) );
- //====================================================
- 
- //Enviando o email 
- //==================================================== 
- if (mail ($email_destinatario, $email_assunto, nl2br($email_conteudo), $email_headers)){ 
- echo "</b>E-Mail enviado com sucesso!</b>"; 
- } 
- else{ 
- echo "</b>Falha no envio do E-Mail!</b>"; } 
- //====================================================
-} 
+if(isset($_POST['enviar'])) {
+
+	// Variáveis do formulário
+	$nome = $_POST['nome'];
+	$email = $_POST['email'];
+	$mensagem = $_POST['mensagem'];
+
+	// Destinatário e assunto do e-mail
+	$para = "marceloaggio10@gmail.com";
+	$assunto = "Mensagem do formulário de contato";
+
+	// Corpo do e-mail
+	$corpo = "Nome: ".$nome."\n".
+			 "E-mail: ".$email."\n".
+			 "Mensagem: ".$mensagem;
+
+	// Cabeçalhos do e-mail
+	$cabecalhos = "From: remetente@example.com"."\r\n".
+				  "Reply-To: ".$email."\r\n".
+				  "X-Mailer: PHP/".phpversion();
+
+	// Enviar o e-mail
+	if(mail($para, $assunto, $corpo, $cabecalhos)) {
+		echo "E-mail enviado com sucesso!";
+	} else {
+		echo "Erro ao enviar o e-mail.";
+	}
+
+}
 ?>
